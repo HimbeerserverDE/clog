bde0650600ca4390978f4904e32a823c75360a0f
HimbeerserverDE <himbeerserverde@gmail.com>
2022-04-24 21:43:39 +0200

    Add lprintf function

---
+++ clog.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "clog.h"
+
+#define TIMESTRLEN 20
+
+int lprintf(const char *format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+
+	time_t timer = time(NULL);
+	struct tm *tm_info = localtime(&timer);
+
+	char buf[TIMESTRLEN];
+	strftime(buf, TIMESTRLEN, "%Y/%m/%d %H:%M:%S", tm_info);
+	free(tm_info);
+
+	int n = printf("%s ", buf);
+
+	size_t fmtlen = strlen(format) + 2;
+	char fmt[fmtlen];
+
+	memset(fmt, '\0', fmtlen);
+	strcpy(fmt, format);
+	strcat(fmt, "\n");
+
+	n += vprintf(format, ap);
+
+	va_end(ap);
+	return n;
+}
